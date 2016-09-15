f52ca0236b827fc77fdca576690cab119b7affe1
azrebiec <andrew.zrebiec@rutgers.edu>
2016-09-15 10:15:59 -0400

    added force and movement to PlayerController

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement);
 	}
 }
